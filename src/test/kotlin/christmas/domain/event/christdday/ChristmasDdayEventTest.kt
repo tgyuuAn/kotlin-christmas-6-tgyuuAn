56ef96fcc3b844dd750e5b9d8f69b1b35178cb5d
tgyuu-An <atg0614@gmail.com>
2023-11-14 00:34:43 +0900

    test : ChristmasDday Evet Test Code 추가

--- src/test/kotlin/christmas/domain/event/christdday/ChristmasDdayEventTest.kt
+++ src/test/kotlin/christmas/domain/event/christdday/ChristmasDdayEventTest.kt
@@ -4,6 +4,7 @@ import christmas.domain.MenuItem
 import christmas.domain.Reservation
 import christmas.domain.event.ChristmasDdayEvent
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -36,6 +37,34 @@ class ChristmasDdayEventTest {
 
         //then
         val actual = christmasDdayEvent.isEligibleForEvent()
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `총 음식 예약 금액이 10000원 이하일 경우 할인 혜택을 받지 못한다`() {
+        //given
+        val reservation =
+            Reservation(listOf(MenuItem.MUSHROOM_SOUP), 3)
+        val christmasDdayEvent = ChristmasDdayEvent(reservation)
+
+        //when
+        val actual = christmasDdayEvent.isEligibleForEvent()
+
+        //then
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `총 음식 예약 금액이 10000원 이상이고, 25일 이하면 할인 혜택을 받을 수 있다`() {
+        //given
+        val reservation =
+            Reservation(listOf(MenuItem.MUSHROOM_SOUP,MenuItem.MUSHROOM_SOUP), 3)
+        val christmasDdayEvent = ChristmasDdayEvent(reservation)
+
+        //when
+        val actual = christmasDdayEvent.isEligibleForEvent()
+
+        //then
         assertThat(actual).isTrue()
     }
 
