3ec5baea2f4aaf7e72cdb9904bc638ad2374d741
tgyuu-An <atg0614@gmail.com>
2023-11-15 14:17:54 +0900

    feature : 총 혜택 금액 0원일 경우 앞에 - 안붙도록 수정

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -20,12 +20,12 @@ class OutputView {
         printDecemberEventBadge(calculatedBenefit)
     }
 
-    fun printWelcomeMessage(reservation: Reservation) {
+    private fun printWelcomeMessage(reservation: Reservation) {
         println("12월 ${reservation.visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
         println()
     }
 
-    fun printMenus(reservation: Reservation) {
+    private fun printMenus(reservation: Reservation) {
         println("<주문 메뉴>")
         reservation.orderedMenus.forEach { orderedMenu ->
             printMenu(orderedMenu)
@@ -39,13 +39,13 @@ class OutputView {
         println("${menuName} ${orderedCount}개")
     }
 
-    fun printTotalAmount(orderedMenus: Reservation) {
+    private fun printTotalAmount(orderedMenus: Reservation) {
         println("<할인 전 총주문 금액>")
         println("-" + decimalFormat.format(orderedMenus.getTotalAmount()) + "원")
         println()
     }
 
-    fun printFreebie(benefit: Benefit) {
+    private fun printFreebie(benefit: Benefit) {
         println("<증정 메뉴>")
         if (benefit.discountedAmount[FREEBIE] == CHAMPAGNE.price) {
             println("${CHAMPAGNE.displayName} 1개")
@@ -56,7 +56,7 @@ class OutputView {
         println()
     }
 
-    fun printTotalBenefitDetail(benefit: Benefit) {
+    private fun printTotalBenefitDetail(benefit: Benefit) {
         println("<혜택 내역>")
         if (benefit.discountedAmount.size == 0) {
             println("없음")
@@ -66,19 +66,26 @@ class OutputView {
         println(benefit.toString())
     }
 
-    fun printTotalBenefit(benefit: Benefit) {
+    private fun printTotalBenefit(benefit: Benefit) {
+        val totalDiscountedAmount = benefit.getTotalDiscountedAmount()
         println("<총혜택 금액>")
-        println("-" + decimalFormat.format(benefit.getTotalDiscountedAmount()) + "원")
+        if (totalDiscountedAmount > 0) {
+            println("-" + decimalFormat.format(benefit.getTotalDiscountedAmount()) + "원")
+            println()
+            return
+        }
+
+        println(decimalFormat.format(benefit.getTotalDiscountedAmount()) + "원")
         println()
     }
 
-    fun printExpectedPaymentAmount(reservation: Reservation, benefit: Benefit) {
+    private fun printExpectedPaymentAmount(reservation: Reservation, benefit: Benefit) {
         println("<할인 후 예상 결제 금액>")
         println(decimalFormat.format(reservation.getTotalAmount() - benefit.getTotalDiscountedAmount()) + "원")
         println()
     }
 
-    fun printDecemberEventBadge(benefit : Benefit) {
+    private fun printDecemberEventBadge(benefit : Benefit) {
         println("<12월 이벤트 배지>")
         val badge = getBadge(benefit)
         println(badge.badgeName)
