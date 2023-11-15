dd96d7780f45b0b6966c98c90bd52f8b33e3edf6
tgyuu-An <atg0614@gmail.com>
2023-11-15 19:51:12 +0900

    feature : 프로젝트 완성

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -8,7 +8,7 @@ import christmas.util.StringFormatter.decimalFormat
 
 class OutputView {
     fun printPreviewMessage(){
-        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.)")
+        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
     }
 
     fun printReservationDetails(reservation: Reservation, calculatedBenefit: Benefit) {
